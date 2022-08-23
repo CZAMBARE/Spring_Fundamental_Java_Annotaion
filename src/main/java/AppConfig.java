2e49673c2d06298b3ea817b4f48167dbb6541861
CZAMBARE <zambare.chetan123@gmil.com>
2022-08-23 15:29:02 +0530

    Component Anotation, We are moving from Bean Annotation to Compoenet Annotation Hence It helped to not to write code at App.config and As Componet Annoation is class level Annoation It is creating Bean of that class

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,12 +4,15 @@ import com.spring.service.SpeakerService;
 import com.spring.service.SpeakerServiceImpl;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan({"com.spring"})
 public class AppConfig {
 
+    /*
     @Bean(name="speakerService")
     @Scope(value = BeanDefinition.SCOPE_SINGLETON)        //@Scope(value ="singleton")   // If Nothing Mentioned It is Singleton by defualt.
     //@Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
@@ -22,12 +25,13 @@ public class AppConfig {
 
         return service;
     }
+    */
 
-
+    /*
     @Bean(name="speakerRepository")
     public SpeakerRepository getSpeakerRepository(){
         return new HibernetSpeakerRepositoryImpl();
-    }
+    }*/
 
 
 
--- src/main/java/com/spring/service/SpeakerServiceImpl.java
+++ src/main/java/com/spring/service/SpeakerServiceImpl.java
@@ -4,14 +4,19 @@ import com.spring.model.Speaker;
 import com.spring.repository.HibernetSpeakerRepositoryImpl;
 import com.spring.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
+@Service("speakerService")
+@Scope(value = BeanDefinition.SCOPE_SINGLETON)
 public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
 
+    @Autowired     // Autowired with Constuctor Constructor
     public SpeakerServiceImpl (SpeakerRepository speakerRepository){
         System.out.println("Spreaker ServiceImpl repository constuctors");
         repository = speakerRepository;
@@ -26,7 +31,7 @@ public class SpeakerServiceImpl implements SpeakerService {
 
     }
 
-    @Autowired
+    /// @Autowired   - Autowired With Setter Injection
     public void setRepository(SpeakerRepository repository) {
         System.out.println("Spreaker ServiceImpl Setter");
         this.repository = repository;
